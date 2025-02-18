a77a1f9df7ee40e1784440839afa1bfc3243cdb2
Vasyliazhenko Dmytro Vitaliyovych <demon4225@gmail.com>
2025-02-18 13:55:17 +0200

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/GitHub-Kanban/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
